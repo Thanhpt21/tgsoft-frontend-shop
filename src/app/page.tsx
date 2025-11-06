a32afa4250ec341545b59575d230d7574b58e674
MinhTran <tcminh698@gmail.com>
2025-11-06 15:26:29 +0700

    update brerum

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import {FlashDeals} from "@/components/layout/home"
 import {TopCategories} from "@/components/layout/home"
 import {ProductList} from "@/components/layout/home"
 import {TopFooter} from "@/components/layout/home"
+import {Thuonghieu} from "@/components/layout/home"
 
 
 export default function Page() {
@@ -13,6 +14,7 @@ export default function Page() {
       <FlashDeals />
       <TopCategories />
       <ProductList />
+      <Thuonghieu/>
       <TopFooter />
     </main>
   )
